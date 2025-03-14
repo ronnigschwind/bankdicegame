11bf2937daeeacff46c095695de2f5699642b7e0
Ronni Gschwind <ronnigschwind@gmail.com>
2025-03-14 10:59:01 -0400

    Fixed issues with builds and removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Make sure you're importing from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// Use the new `createRoot` API
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';  // Make sure you're importing from 'react-dom/client'
+// import './index.css';
+// import App from './App';
+
+// // Use the new `createRoot` API
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
